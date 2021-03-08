3e7f8ab3a15d2d10fc72453fa9d4e5f72cb44a28
Changdae <einsvision@gmail.com>
2021-03-08 10:36:21 +0900

    made structure whole web pages

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="app">
-      <h1>Airbnb-Clone✨🚀</h1>
       {/* Home */}
-        {/* Header */}
+      <Header />
+      <Home />
         {/* Banner -> search page */}
           {/* Search */}
         {/* Cards */}
         {/* Footer */}
+
       {/* Search Page */}
+        {/* Header */}
         {/* ... */}
     </div>
   );
