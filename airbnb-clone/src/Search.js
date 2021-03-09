d341e0d49474a90b61cfd615a189d41d5d901cea
Changdae <einsvision@gmail.com>
2021-03-09 11:17:14 +0900

    building search function

--- airbnb-clone/src/Search.js
+++ airbnb-clone/src/Search.js
@@ -3,6 +3,8 @@ import './Search.css';
 import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
+import PeopleIcon from '@material-ui/icons/People';
+import { Button } from '@material-ui/core';
 
 function Search() {
   const [startDate, setStartDate] = useState(new Date());
@@ -25,6 +27,9 @@ function Search() {
         ranges={[selectionRange]}
         onChange={handleSelect}
       />
+      <h2>Number of guests <PeopleIcon/> </h2>
+      <input min={0} defaultValue={2} type="number"/>
+      <Button>Search Airbnb</Button>
     </div>
   )
 }
