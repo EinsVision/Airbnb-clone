768514d97978abd04c874750de30c49f802f9454
Changdae <einsvision@gmail.com>
2021-03-09 12:44:45 +0900

    built card component to show informations

--- airbnb-clone/src/Home.js
+++ airbnb-clone/src/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Banner from './Banner';
+import Card from './Card';
 import './Home.css';
 
 function Home() {
   return (
     <div className='home'>
-      <h1>Home component</h1>
       <Banner />
+      <div className="home__section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/4d353c80-e73a-4b04-9e15-ec3d8381b106.jpg?im_w=720"
+          title="Online Experiences"
+          description="Unique activities we can do together, led by a world of hosts."
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/e81fce5f-2f51-4342-938e-5bc18ae237f4.jpg?im_w=720"
+          title="Unique stays"
+          description="Spaces that are more than just a place to sleep."
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/b9adfc39-6e2a-4e5f-b6f3-681b306fae5c.jpg?im_w=720"
+          title="Entire homes"
+          description="Comfortable private places, with room for friends or family."
+        />
+      </div>
     </div>
   )
 }
