bdc31029f42999a68683201587ba3b07c38086a7
Changdae <einsvision@gmail.com>
2021-03-09 12:51:33 +0900

    display cards to show rooms

--- airbnb-clone/src/Home.js
+++ airbnb-clone/src/Home.js
@@ -24,6 +24,47 @@ function Home() {
           description="Comfortable private places, with room for friends or family."
         />
       </div>
+      <div className="home__section">
+        <Card
+          src="https://a0.muscache.com/im/pictures/1234059/492861b7_original.jpg?im_w=960"
+          title="3 Bedroom Flat in Cicago"
+          description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+          price="$130/night"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/3315684/a97df0d5_original.jpg?im_w=960"
+          title="Penthouse in CA"
+          description="Enjoy the amazing sights of London with this stunning penthouse"
+          price="$250/night"
+        />
+        <Card
+          src="https://a0.muscache.com/im/pictures/13114457/65b2fd17_original.jpg?im_w=960"
+          title="1 Bedroom apartment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="$270/night"
+        />
+      </div>
+
+      <div className="home__section">
+        <Card
+          src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+          title="3 Bedroom Flat in Bournemouth"
+          description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+          price="$130/night"
+        />
+        <Card
+          src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+          title="Penthouse in London"
+          description="Enjoy the amazing sights of London with this stunning penthouse"
+          price="$350/night"
+        />
+        <Card
+          src="https://media.nomadicmatt.com/2018/apartment.jpg"
+          title="1 Bedroom apartment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="$70/night"
+        />
+      </div>
     </div>
   )
 }
