fc55eb752bd2687b85c6c5775983e24e10e9fcd8
Changdae <einsvision@gmail.com>
2021-03-09 10:45:41 +0900

    installed dependency date-fns

--- airbnb-clone/src/Search.js
+++ airbnb-clone/src/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Search.css';
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 function Search() {
   return (
