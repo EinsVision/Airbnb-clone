5b3d77efea869ad2cb4247d84230bd88722c9eed
Changdae <einsvision@gmail.com>
2021-03-10 15:52:19 +0900

    made homepage structure using react-router-dom

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SearchPage from './SearchPage';
 
 function App() {
   return (
     <div className="app">
       {/* Home */}
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/search'>
+            <SearchPage />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
+      
         {/* Banner -> search page */}
           {/* Search */}
         {/* Cards */}
